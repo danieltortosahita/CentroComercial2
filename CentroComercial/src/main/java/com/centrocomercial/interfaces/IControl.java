d20b5dde571fba92b187ddd7f3f44cfe04d3849f
Ana <ab.rodriguezserrano@gmail.com>
2021-07-28 20:55:38 +0200

    modificadas y documentadas las Interfaces del proyecto

--- CentroComercial/src/main/java/com/centrocomercial/interfaces/IControl.java
+++ CentroComercial/src/main/java/com/centrocomercial/interfaces/IControl.java
@@ -1,14 +1,34 @@
-/*
- * 
+/**
+ *Paquete específico para las interface del proyecto centroComercial.java
  */
 package com.centrocomercial.interfaces;
 
 /**
- * 
- * 
+ *
+ * @author
  */
 public interface IControl {
-    public void abrirPaso(String id);
-    public void cerrarPaso(String id);
-    public void controlPersonas(String id); 
+
+    /**
+     *
+     * @param id de la superficie del centro comercial en la que nos encontremos
+     * @return nos devuelve valor de tipo String.
+     */
+    public String abrirPaso(String id);
+
+    /**
+     *
+     * @param id de cada superficie del centro comercial en la que nos
+     * encontremos
+     * @return nos devuelve un valor de tipo String.
+     */
+    public String cerrarPaso(String id);
+
+    /**
+     *
+     * @param id de cada superficie del centro comercial
+     * @return nos devulve un valor entero para indicar el control de personas
+     * que hay en ese lugar concreto.
+     */
+    public int controlPersonas(String id);
 }
--- CentroComercial/src/main/java/com/centrocomercial/interfaces/IReparaciones.java
+++ CentroComercial/src/main/java/com/centrocomercial/interfaces/IReparaciones.java
@@ -1,13 +1,25 @@
 /*
- * 
+ * Paquete específico para las interface del proyecto centroComercial.java
  */
 package com.centrocomercial.interfaces;
 
 /**
- *
- * 
+ * Interface IReparaciones de la que implementarán las clases hijas de la clase
+ * padre Edificio.
  */
 public interface IReparaciones {
-    public void reparar(String id);
-    public void limpiar(String id);
+
+    /**
+     * @param id de cada superficie del centro comercial ya sea local, aseo,
+     * acceso o parking.
+     * @return nos devuelve un valor de tipo String.
+     */
+    public String reparar(String id);
+
+    /**
+     * @param id de cada superficie del centro comercial ya sea local, aseo,
+     * acceso o parking.
+     * @return nos devuelve un valor de tipo String.
+     */
+    public String limpiar(String id);
 }
