8d429bb610d90fef1e86a445620a5a03b321281f
Ana <ab.rodriguezserrano@gmail.com>
2021-07-28 19:09:50 +0200

    Creación  de interfaces y clase Aseos

--- CentroComercial/src/main/java/com/centrocomercial/interfaces/IControl.java
+++ CentroComercial/src/main/java/com/centrocomercial/interfaces/IControl.java
@@ -1,14 +1,14 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * 
  */
 package com.centrocomercial.interfaces;
 
 /**
- *
- * @author Dani
+ * 
+ * 
  */
 public interface IControl {
-    
+    public void abrirPaso(String id);
+    public void cerrarPaso(String id);
+    public void controlPersonas(String id); 
 }
