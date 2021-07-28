7ea2e96245d2e19975e1d214e014ac6be51ef964
Daniel Tortosa <danieltortosahita@gmail.com>
2021-07-28 19:02:46 +0200

    Añadida la clase padre llamada Edificio.

---
+++ CentroComercial/src/main/java/com/centrocomercial/clases/Edificio.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.centrocomercial.clases;
+
+/**
+ *
+ * @author Dani
+ */
+public class Edificio {
+
+    private String id, horarioApertura, horarioCierre;
+    private boolean disponible, abierto;
+    private int numeroPlantas;
+
+    public String horarioApertura() {
+        return "";
+    }
+
+    public String indicativoLugar() {
+        return "";
+    }
+}
