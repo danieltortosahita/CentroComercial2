ea33ea2a1a5380a82d70d434d8c92ea210f870d7
José Carlos <jc.cruzado.alonso@gmail.com>
2021-07-28 23:13:31 +0200

    Modificaciones de Accesos y Control de Usuarios

--- CentroComercial/src/main/java/com/centrocomercial/accesos/Accesos.java
+++ CentroComercial/src/main/java/com/centrocomercial/accesos/Accesos.java
@@ -1,60 +1,18 @@
 /*
- * Esta clase será utilizada para controlar la creación y acceso de usuarios
- * a la aplicación.
+ * Esta clase será utilizada para controlar los accesos al centro comercial
  */
 package com.centrocomercial.accesos;
-
-import java.util.HashMap;
-import java.util.Map;
+import com.centrocomercial.CentroComercial;
 
 /**
  *
  * @author José Carlos Cruzado
  */
-public class Accesos {
+public class Accesos extends CentroComercial {
+
+    private int numeroPlanta;
+    private String tipoAcceso;
+    private boolean vigilado;
     
-    private HashMap <String, String> usuarios = new HashMap();
-
-    public Accesos() {
-    }
-
-    public HashMap<String, String> getUsuarios() {
-        return usuarios;
-    }
-
-    public void setUsuarios(HashMap<String, String> usuarios) {
-        this.usuarios = usuarios;
-    }
-
-    /**
-     * Este método sirve para añadir a un nuevo usuario
-     * @param nombre Es un argumento tipo String con el nombre del usuario
-     * @param pass Es un argumento tipo String con la contraseña del usuario
-     * @return True o False, dependiendo de si ha podido o no añadir al usuario
-     */
-    public boolean addUsuario (String nombre, String pass) {
-        int tamanioAntiguo = usuarios.size();
-        usuarios.put(pass, pass);
-        if (tamanioAntiguo == (usuarios.size() - 1))
-            return true;
-        else
-            return false;
-    }
     
-    /**
-     * Este método sirve para comprobar en el HashMap que el usuario está dado de alta
-     * @param nombre Argumento tipo String con el nombre del usuario
-     * @param pass Argumento tipo String con la contraseña del usuario
-     * @return True o False, dependiendo de si está en el HashMap o no
-     */
-    public boolean checkUsuario (String nombre, String pass) {
-        boolean accesoPermitido = false;
-        
-        for (Map.Entry<String, String> entry : usuarios.entrySet()) {
-            if ((nombre.equals(entry.getKey())) && (pass.equals(entry.getValue())))
-                accesoPermitido = true;
-        }
-        
-        return accesoPermitido;
-    }
 }
